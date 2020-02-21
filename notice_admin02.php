8b839efdd0b4211dd93525c7534bc2fd2067417e
karmpatel216 <61292315+karmpatel216@users.noreply.github.com>
2020-02-21 21:42:39 +0530

    upload Notice page(student & Admin) with header

--- notice_admin02.php
+++ notice_admin02.php
@@ -1,5 +1,5 @@
 <?php
-include 'dbconnection01.php';
+include 'dbconnection1.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,9 +10,10 @@ include 'dbconnection01.php';
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <link rel="icon" href="img/fav-icon.png" type="image/x-icon" />
+       <link rel="icon" href="img/vgeclogo.png" type="image/x-icon" />
+
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>HillTown Resort</title>
+        <title>Hostel Management</title>
 
         <!-- Icon css link -->
         <link href="css/font-awesome.min.css" rel="stylesheet">
@@ -41,7 +42,25 @@ include 'dbconnection01.php';
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+
         <style type="text/css">
+            <style type="text/css">
+                    .resort_title1 h2 {
+  font-size: 27px;
+  font-weight: bold;
+  font-family: "Montserrat", sans-serif;
+  letter-spacing: .96px;
+  text-transform: uppercase;
+  color: #242424;
+  margin-top: 20px;
+  padding-bottom: 10px;
+}
+
+.resort_title1 h2 span {
+  color: #ffb606;
+
+  padding-left: 20px;
+}
         	.form-control textarea
         	{
         		color: black;
@@ -63,115 +82,7 @@ include 'dbconnection01.php';
     <body>
 
         <!--================Header Area =================-->
-        <header class="main_header_area">
-            <div class="header_top">
-                <div class="container">
-                    <div class="header_top_inner">
-                        <div class="pull-left">
-                            <a href="#"><i class="fa fa-phone"></i>+ (1800) 456 7890</a>
-                            <a href="#"><i class="fa fa-envelope-o"></i>info@hilltownresort.com</a>
-                        </div>
-                        <div class="pull-right">
-                            <ul class="header_social">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>            </div>
-            <div class="header_menu">
-                <nav class="navbar navbar-default">
-                    <div class="container">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="#">
-                                <img src="img/logo.png" alt="">
-                                <img src="img/logo-sticky.png" alt="">
-                            </a>
-                        </div>
-
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul class="nav navbar-nav">
-                                <li class="dropdown submenu">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Home <i class="fa fa-chevron-down" aria-hidden="true"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="index.html">Home Page 1</a></li>
-                                        <li><a href="index-2.html">Home Page 2</a></li>
-                                        <li><a href="index-3.html">Home Page 3</a></li>
-                                        <li><a href="index-4.html">Home Page 4</a></li>
-                                    </ul>
-                                </li>
-                                <li class="submenu dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Rooms <i class="fa fa-chevron-down" aria-hidden="true"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="room.html">Rooms Grid Style</a></li>
-                                        <li><a href="room-list.html">Rooms List Style</a></li>
-                                        <li><a href="room-details.html">Single Room</a></li>
-                                    </ul>
-                                </li>
-                                <li class="submenu dropdown active">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">pages <i class="fa fa-chevron-down" aria-hidden="true"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="aminities.html">Aminities</a></li>
-                                        <li><a href="search.html">Search</a></li>
-                                        <li class="dropdown submenu">
-                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Events</a>
-                                            <ul class="dropdown-menu">
-                                                <li><a href="event.html">-  Event List</a></li>
-                                                <li><a href="event-two-column.html">-  Event Grid</a></li>
-                                                <li><a href="event-details.html">-  Event Single</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="dropdown submenu">
-                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gallery</a>
-                                            <ul class="dropdown-menu">
-                                                <li><a href="gallery-two-column.html">-  Gallery 2 Column</a></li>
-                                                <li><a href="gallery-three-column.html">-  Gallery 3 Column</a></li>
-                                                <li><a href="cobage-gallery.html">-  Gallery Cobage</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="restaurant.html">Our Reataurant</a></li>
-                                        <li><a href="spa.html">Spa & Relax</a></li>
-                                        <li><a href="activities.html">Activities</a></li>
-                                        <li><a href="comming-soon.html">Coming Soon page</a></li>
-                                        <li><a href="404.html">404 Error</a></li>
-                                    </ul>
-                                </li>
-                                <li class="submenu dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog <i class="fa fa-chevron-down" aria-hidden="true"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="blog-left-sidebar.html">Blog with leftside bar</a></li>
-                                        <li><a href="blog-right-sidebar.html">Blog with rightside bar</a></li>
-                                        <li><a href="blog.html">Blog without side bar</a></li>
-                                        <li><a href="blog-details.html">Blog details</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="about-us.html">About Us</a></li>
-                                <li><a href="contact-us.html">Contact Us</a></li>
-                            </ul>
-                            <ul class="nav navbar-nav navbar-right">
-                                <li class="search_dropdown">
-                                    <a class="popup-with-zoom-anim" href="#test-search"><i class="icon icon-Search"></i></a>
-                                </li>
-                                <li class="book_btn">
-                                    <a class="book_now_btn" href="#">Book now</a>
-                                </li>
-                            </ul>
-                        </div><!-- /.navbar-collapse -->
-                    </div><!-- /.container-fluid -->
-                </nav>
-            </div>
-        </header>
-        <!--================Header Area =================-->
+       <?php include 'header_admin.php'; ?>
         
         <!--================Banner Area =================-->
         <section class="banner_area">
@@ -190,7 +101,7 @@ include 'dbconnection01.php';
             error_reporting(E_ERROR | E_WARNING | E_PARSE);
             $submit=$_POST['submit'];
             if($submit!=='submit'){                
-                $error=mysqli_error($connection);
+                $error=mysqli_error($connection1);
                 echo $error;}
             if($submit=='submit'){
 
@@ -200,9 +111,9 @@ include 'dbconnection01.php';
             $notice=$_POST['notice'];
             $status=1;
             $query="insert into notice (subject,sdate,edate,notice,status) values ('$subject','$sdate','$edate','$notice','$status')";
-            $run=mysqli_query($connection,$query);
+            $run=mysqli_query($connection1,$query);
             if(!$run){
-                           $error= mysqli_error($connection);
+                           $error= mysqli_error($connection1);
                            echo $error;}
             }
 ?>
@@ -214,10 +125,10 @@ include 'dbconnection01.php';
                             $not=$_POST['not'];
                             $nid=$_POST['nid'];
                             $query="update notice set status='$status' where subject='$sub' and notice='$not' and nid='$nid'";
-                            $run=mysqli_query($connection,$query);
+                            $run=mysqli_query($connection1,$query);
                             if(!$run)
                             {  
-                                $error=mysqli_error($connection);
+                                $error=mysqli_error($connection1);
                                 echo "$error";
                             }
                         }
@@ -229,26 +140,27 @@ include 'dbconnection01.php';
         	<div class="container" style="width: 70%;background: white;height: 85%;margin: all;padding: 20px;" >
                 <div class="container" style="width: 80%;background: #E3E4FA;height: 95%;padding-bottom: 30px;margin:auto;margin-top: 15px;overflow-y: scroll;">  
                 <?php 
-                    $query="select subject,notice,sdate,edate,status,nid from notice";
-                    $run=mysqli_query($connection,$query);
+                    $query="select subject,notice,sdate,edate,status,nid,timestamp from notice";
+                    $run=mysqli_query($connection1,$query);
                     if(!$run)
                     {
-                        $error=mysqli_error($connection);
+                        $error=mysqli_error($connection1);
                         echo "$error";
                     }
                     while($result=mysqli_fetch_array($run,MYSQLI_NUM)){
                         if($result[4]=='1' && $result[2]<=date("Y-m-d") && $result[3]>=date("Y-m-d")){ 
                 ?>
                 <div><form method="post" enctype="multipart/form-data">
-                    <p style="background: none;border: none;font-size: 15px;padding: 15px;margin: 15px;color: black;"><span style="font-weight: bold;padding-bottom: 0px;"><?php echo "$result[0]".":"?></span><?php echo "$result[1]"?></p>
-                    <pre style="background: none;border: none;font-size: 15px;padding: 15px;margin: 15px;padding-top: 0px;color: grey;"><span style="font-weight: bold;">Expires on:</span><?php echo "$result[3]"?>
-                    <button type="submit" class="btn btn-default btn-sm" name="remove"><span class="glyphicon glyphicon-remove"></span></button>
+                    <p style="background: none;border: none;font-size: 15px;margin: 15px;color: black;"><span style="font-weight: bold;"><?php echo "$result[0]".":"?></span><?php echo "$result[1]"?></p>
+                    <span style="margin: 15px;"><?php echo "$result[6]" ?></span>
+                    <pre style="background: none;border: none;font-size: 15px;margin:10px;color: grey;"><span style="font-weight: bold;">Expires on:</span><?php echo "$result[3]"?>
+                    <button type="submit" class="btn btn-default btn-sm" name="remove" ><span class="glyphicon glyphicon-remove"></span></button></div>
                     <input type="hidden" name="remove" value="remove">
                     <input type="hidden" name="sub" value="<?php echo $result[0]; ?>">
                     <input type="hidden" name="not" value="<?php echo $result[1]; ?>">    
                     <input type="hidden" name="nid" value="<?php echo $result[5]; ?>">
                 </form>    
-                </div>
+                
                 <hr style="border-color: black;">
                 <? }}
                 ?>
@@ -257,7 +169,7 @@ include 'dbconnection01.php';
         </section>
 
         <!--================Footer Area =================-->
-       <?php include 'footer_admin.php'; ?>
+        <?php include 'footer_admin.php'; ?>
         <!--================End Footer Area =================-->
         
         <!--================Search Box Area =================-->
