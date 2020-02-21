4a63448765a241cf210f74b86a3b6285093a151c
malavmevada <51991362+malavmevada@users.noreply.github.com>
2020-02-21 17:36:51 +0530

    Add files via upload
    Admin footer file

---
+++ footer_admin.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<!-- Mirrored from designarc.biz/demos/hilltown/theme/activities.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 09 Dec 2019 15:45:13 GMT -->
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        
+        <link rel="icon" href="fees/img/fav-icon.png" type="image/x-icon" />
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Hostel Managemnet</title>
+
+        <!-- Icon css link -->
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="vendors/stroke-icon/style.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- Rev slider css -->
+        <link href="vendors/revolution/css/settings.css" rel="stylesheet">
+        <link href="vendors/revolution/css/layers.css" rel="stylesheet">
+        <link href="vendors/revolution/css/navigation.css" rel="stylesheet">
+        <link href="vendors/animate-css/animate.css" rel="stylesheet">
+        
+        <!-- Extra plugin css -->
+        <link href="vendors/magnify-popup/magnific-popup.css" rel="stylesheet">
+        <link href="vendors/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+        <link href="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css" rel="stylesheet">
+        <link href="vendors/bootstrap-selector/bootstrap-select.css" rel="stylesheet">
+        <link href="vendors/lightbox/simpleLightbox.css" rel="stylesheet">
+        
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+
+
+             <style >
+        .button{
+  background-color:black;
+  border: none;
+  color: white;
+  padding: 15px 25px;
+  text-align: center;
+  font-size: 16px;
+  cursor: pointer;
+  -webkit-transition-duration: 0.5s;
+  -o-transition-duration: 0.5s;
+  transition-duration: 0.5s;
+}
+
+ .button:hover{
+  background-color: #039287;
+  color: white;
+}
+
+
+
+.activities_area .activities_item_inner .activities_item_row .activities_content p {
+  font-family: "Open Sans", sans-serif;
+  font-size: 17px;
+  line-height: 25px;
+  letter-spacing: .225px;
+  color: #000000;
+  padding:0px 100px 0px 0px;
+}
+
+
+.activities_title .resort_title p{
+  font-size: 22px;
+  font-family: "Open Sans", sans-serif;
+  line-height: 26px;
+  color: #000000;
+  letter-spacing: .225px;
+  padding-top: 30px;
+}
+
+
+.resort_title1 h2 {
+  font-size: 27px;
+  font-weight: bold;
+  font-family: "Montserrat", sans-serif;
+  letter-spacing: .96px;
+  text-transform: uppercase;
+  color: #242424;
+  
+  padding-bottom: 20px;
+}
+
+.resort_title1 h2 span {
+  color: #ffb606;
+  margin-top: 20px;:
+}
+
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.desc {
+  padding: 15px;
+  text-align: center;
+}
+
+
+</style>
+</head>
+<body>
+              <!--================Header Area =================-->
+      
+
+        <footer class="footer_area">
+            <div class="footer_widget_area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-xs-6">
+                            <aside class="f_widget about_widget">
+                                <!-- <div class="h_middle_text2">
+                             <a href="#"><img src="img/logo.png" alt=""></a> 
+                            < <h1><span id="H">H</span>OSTEL</h1> <h3> MANAGEMEN<span id="T">T</span></h3>
+                        </div> --> 
+                        <div>
+                                <a class="navbar-brand" href="#">
+                                <img src="img/logo.png" alt="" height="70px" >
+                               
+                            </a>
+                           
+                            <div class="resort_title1">
+                                <h2 style="padding-top: 20px;"><span style="color: #039287">HOSTEL <span style="color: #039287">MANAGEMENT</span></span></h2>
+                            </div>
+                             </div>
+                       
+                        
+                           
+                           
+                      
+
+
+                                <div class="ab_wd_list">
+                                    <div class="media">
+                                        <div class="media-left">
+                                            <i class="fa fa-map-marker"></i>
+                                        </div>
+                                        <div class="media-body">
+                                            <h4 style="color: white;">VGEC Hostel, Chandkheda <br /> 382424,Gandhinagar</h4>
+                                        </div>
+                                    </div>
+                                    <div class="media">
+                                        <div class="media-left">
+                                            <i class="fa fa-envelope-o"></i>
+                                        </div>
+                                        <div class="media-body">
+                                           <a href="#"> <h4 style="color: white; font-size: 10px; ">hostelmanagement01@gmail.com</h4></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="book_now_area">
+                                    <a class="book_now_btn" href="#">Login</a>
+                                </div>
+                            </aside>
+                        </div>
+                        <div class="col-md-4 col-xs-6">
+                            <aside class="f_widget link_widget">
+                                <div class="f_title">
+                                    <h3>Useful Links</h3>
+                                </div>
+                                <ul>
+                                    <li><a href="#">- Sign up</a></li>
+                                    <li><a href="#">- Admission process</a></li>
+                                    <li><a href="#">-  Gallery</a></li>
+                                    <li><a href="help.html">-  Faq’s </a></li>
+                                    <li><a href="https://www.vgecg.ac.in/" target="_blank" >-  visit our college web</a></li>
+                                </ul>
+                            </aside>
+                        </div>
+                        <!-- <div class="col-md-2 col-xs-6">
+                            <aside class="f_widget link_widget">
+                                <div class="f_title">
+                                    <h3>our services</h3>
+                                </div>
+                                <ul>
+                                    <li><a href="#">-  Food & Drinks</a></li>
+                                    <li><a href="#">-  Rooms</a></li>
+                                    <li><a href="#">-  Amenities</a></li>
+                                    <li><a href="#">-  Spa & Gym</a></li>
+                                    <li><a href="#">-  Hill Tours</a></li>
+                                </ul>
+                            </aside>
+                        </div> -->
+                        <div class="col-md-4 col-xs-6">
+                                
+
+                                <div class="f_title">
+                                     <h3 style="margin-right:10px; ">Location</h3>
+                                    
+                                
+                                   
+                                </div> 
+                                
+                            <!-- <aside class="f_widget instagram_widget">
+                                <div class="f_title">
+                                    <h3>Instagram</h3>
+                                </div>
+                                <ul class="instagram_list" id="instafeed"></ul>
+                            </aside> -->
+                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3669.717209126576!2d72.59106171444378!3d23.10744571895034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e83ca44f7d0dd%3A0xbbfdcfcf4fb6ccf0!2sVGEC%20Boys%20Hostel-1!5e0!3m2!1sen!2sin!4v1577121123820!5m2!1sen!2sin" width="100%" height="300px" style=" border:5; border-radius: 4px; opacity: .5;" allowfullscreen=""></iframe>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer_copyright_area">
+                <div class="container">
+                    <div class="pull-left">
+                        <h4>Copyright © Hostel Management  <script>document.write(new Date().getFullYear());</script>. All rights reserved. </h4>
+                    </div>
+                    <div class="pull-right">
+                        <h4>Created by: <a href="#">CE Dept.</a></h4>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--================End Footer Area =================-->
+        
+        <!--================Search Box Area =================-->
+        <div class="search_area zoom-anim-dialog mfp-hide" id="test-search">
+            <div class="search_box_inner">
+                <h3>Search</h3>
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="button"><i class="icon icon-Search"></i></button>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <!--================End Search Box Area =================-->
+        
+        
+        
+        
+        
+        <!--================End Footer Area =================-->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/jquery-2.2.4.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Rev slider js -->
+        <script src="vendors/revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script src="vendors/revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.video.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script src="vendors/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        
+        <script src="vendors/magnify-popup/jquery.magnific-popup.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope.pkgd.min.js"></script>
+        <script src="vendors/counterup/waypoints.min.js"></script>
+        <script src="vendors/counterup/jquery.counterup.min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.js"></script>
+        <script src="Vendors/bootstrap-selector/bootstrap-select.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        
+        <!--gmaps Js-->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+        <script src="js/gmaps.min.js"></script>
+        
+        <!-- instafeed-->
+        <script type="text/javascript" src="fees/vendors/instafeed/instafeed.min.js"></script>
+        <script type="text/javascript" src="fees/vendors/instafeed/script.js"></script>
+        
+        <script src="fees/js/theme.js"></script>
+
+</body>
